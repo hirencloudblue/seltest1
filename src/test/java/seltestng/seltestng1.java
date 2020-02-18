7c69bb0bac953613ff25107692705f0e994b7f23
root <root@ansible-acm>
2020-02-18 16:38:17 -0500

    CHANGE SUCCESSFUL

--- src/test/java/seltestng/seltestng1.java
+++ src/test/java/seltestng/seltestng1.java
@@ -3,34 +3,38 @@ package seltestng;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
+import org.testng.Assert;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 // import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
+
 public class seltestng1 {
- 
-	@Test
-	public static void main(String[] args) 
+ @Test
+	
+	public static void runFromSeltestng()
 	{
 	// System.setProperty("webdriver.chrome.driver","D:\\chromedriver80.exe");
 	System.setProperty("webdriver.gecko.driver", "/root/Downloads/geckodriver");
+	System.out.println("Hi This is a Selenium Test Case");
 	//ChromeOptions chromeOptions = new ChromeOptions();
 	// chromeOptions.addArguments("--headless");
 	// WebDriver driver = new ChromeDriver(chromeOptions);
+
 	WebDriver driver = new FirefoxDriver();
 	driver.get("http://192.168.1.90:3001/");
 	driver.findElement(By.name("login")).sendKeys("devops");
 	driver.findElement(By.name("password")).sendKeys("test");
 	driver.findElement(By.name("click")).click();
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 	String test = driver.findElement(By.name("test")).getText();
 	AssertJUnit.assertEquals(test, "Login Succeeded");
 	System.out.println("Test Succeeded!!");
 	driver.quit();
-		
+
 	}
 }
 	
