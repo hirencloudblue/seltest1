b3b6ba2a211f2a8da7ed6c99620cd9aecdf339e7
root <root@ansible-acm>
2020-02-18 21:52:58 -0500

    Revert to firefox

--- src/test/java/seltestng/seltestng1.java
+++ src/test/java/seltestng/seltestng1.java
@@ -7,7 +7,8 @@ import org.testng.Assert;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -18,17 +19,17 @@ public class seltestng1 {
 	
 	public static void runFromSeltestng()
 	{
-	// System.setProperty("webdriver.chrome.driver","D:\\chromedriver80.exe");
+	 //System.setProperty("webdriver.chrome.driver","/root/Downloads/chromedriver");
 	System.setProperty("webdriver.gecko.driver", "geckodriver");
 	System.out.println("Hi This is a Selenium Test Case");
 	//ChromeOptions chromeOptions = new ChromeOptions();
-	// chromeOptions.addArguments("--headless");
-	// WebDriver driver = new ChromeDriver(chromeOptions);
+	//chromeOptions.addArguments("--headless");
+	 //WebDriver driver = new ChromeDriver(chromeOptions);
 	
 	FirefoxOptions firefoxOptions = new FirefoxOptions();
 	firefoxOptions.setHeadless(true);
-
 	WebDriver driver = new FirefoxDriver();
+
 	driver.get("http://192.168.1.90:3001/");
 	driver.findElement(By.name("login")).sendKeys("devops");
 	driver.findElement(By.name("password")).sendKeys("test");
